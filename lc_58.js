3c3dd8599553366cc5cc02bf1fd40b5efbfd939e
nav-navya <navn0286@gmail.com>
2024-08-28 10:17:28 +0530

    using split and filter

--- lc_58.js
+++ lc_58.js
@@ -1,7 +1,9 @@
 function lastWord(s){
   let arr = s.split(" ");
+  let newArr = arr.filter(x => x!= '');
+  
   let lastWord = arr[arr.length-1];
   console.log(`the last word is '${lastWord}' and length is ${lastWord.length} `) 
 }
 
-lastWord('hello world is');
+lastWord('fly me   to   the moon');
