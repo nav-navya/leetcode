f372b51f2e15bed038b09fa5c73df36f17d2b2f8
nav-navya <navn0286@gmail.com>
2024-10-23 20:44:55 +0530

    closure and methods in js

---
+++ lc_2665.js
@@ -0,0 +1,21 @@
+function chooseyourChoice(num){
+ return {
+  increment:function (num){
+    return num + 1
+  },
+
+  decrement:function (num){
+    return num - 1
+  },
+
+  reset:function (num){
+    return num;
+  }
+} 
+}
+
+const number = chooseyourChoice(9)
+console.log(number.increment(9));
+console.log(number.decrement(9));
+console.log(number.reset(9));
+
