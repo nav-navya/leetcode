09053c55e2e59c3db768a68a7064579656b995c7
nav-navya <navn0286@gmail.com>
2025-01-11 19:00:20 +0530

    check this

---
+++ lc_561.js
@@ -0,0 +1,17 @@
+function arrayPairSum(nums) 
+{
+  nums.sort((a, b)=>a-b);
+
+  let sum=0;
+  
+  
+  for(let i =0; i< nums.length; i += 2) {
+      sum += nums[i];
+  }
+
+  return sum;
+};
+
+console.log(arrayPairSum([1,4,3,2]))
+
+  
